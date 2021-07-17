05cb62356c756d3099f88ffb3bd53c5b00f2fb80
msrecec <67606889+msrecec@users.noreply.github.com>
2021-07-17 07:38:59 +0200

    rewrote sql scripts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,22 @@
 delete from side_effect;
 delete from vaccine;
-delete from user_authority;
-delete from user;
-delete from authority;
+-- delete from user_authority;
+-- delete from user;
+-- delete from authority;
 
-INSERT INTO vaccine (research_name, manufacturer_name, vaccine_type, required_number_of_shots, available_number_of_shots) VALUES
-('BNT162b2', 'Pfizer-BioNTech', 'MRNA', 2, 1000),
-('mRNA-1273', 'Moderna', 'MRNA', 2, 1000),
-('JNJ-78436735', 'Johnson & Johnson', 'VIRAL_VECTOR', 1, 1000),
-('AZD1222', 'AstraZeneca', 'VIRAL_VECTOR', 1, 1000),
-('none', 'Gamaleya Research Institute', 'VIRAL_VECTOR', 1, 1000);
+INSERT INTO manufacturer(manufacturer_name) VALUES
+('Pfizer-BioNTech'),
+('Moderna'),
+('Johnson & Johnson'),
+('AstraZeneca'),
+('Gamaleya Research Institute');
+
+INSERT INTO vaccine (research_name, vaccine_type, required_number_of_shots, available_number_of_shots, manufacturer_id) VALUES
+('BNT162b2', 'MRNA', 2, 1000, 1),
+('mRNA-1273','MRNA', 2, 1000, 2),
+('JNJ-78436735', 'VIRAL_VECTOR', 1, 1000, 3),
+('AZD1222', 'VIRAL_VECTOR', 1, 1000, 4),
+('none', 'VIRAL_VECTOR', 1, 1000, 5);
 
 INSERT INTO side_effect(short_description, long_description, frequency, vaccine_id) VALUES
 ('Glavobolja', 'Jaka bol u prednjem dijelu glave', 3.5, 1),
@@ -19,29 +26,30 @@ INSERT INTO side_effect(short_description, long_description, frequency, vaccine_
 ('Grlobolja', 'Ostra bol u grlu', 6.4, 5);
 
 
+
 /*
 username: admin, password: admin
 username: user, password: user
 username: deleter, password: deleter
 */
 
-INSERT INTO user(username, password, first_name, last_name)
-values ('admin', '$2y$12$CxrCI5stBkAcnO834mtOW.cv9thhHm0Q3ufBmyYJkfgM/J3dy4YFO', 'admin', 'admin');
-
-INSERT INTO user(username, password, first_name, last_name)
-values ('user', '$2y$12$L0MgzFdTtVuQFCaYgIGYiuDD3D0biJs9AhK8R6ilh4PBI1H6bJZ9O', 'user', 'user');
-
-INSERT INTO user(username, password, first_name, last_name)
-values ('deleter', '$2y$12$fZMn4/3ssceFbGEvuZaaheRM8ZmJJsqhtULaf/Ffiq24vcYU7KvSu', 'deleter', 'deleter');
-
-insert into authority(name) values ('ROLE_ADMIN');
-
-insert into authority(name) values ('ROLE_USER');
-
-insert into authority(name) values ('ROLE_DELETER');
-
-insert into user_authority (user_id, authority_id) values (1, 1);
-
-insert into user_authority (user_id, authority_id) values (2, 2);
-
-insert into user_authority (user_id, authority_id) values (3, 3);
+-- INSERT INTO user(username, password, first_name, last_name)
+-- values ('admin', '$2y$12$CxrCI5stBkAcnO834mtOW.cv9thhHm0Q3ufBmyYJkfgM/J3dy4YFO', 'admin', 'admin');
+--
+-- INSERT INTO user(username, password, first_name, last_name)
+-- values ('user', '$2y$12$L0MgzFdTtVuQFCaYgIGYiuDD3D0biJs9AhK8R6ilh4PBI1H6bJZ9O', 'user', 'user');
+--
+-- INSERT INTO user(username, password, first_name, last_name)
+-- values ('deleter', '$2y$12$fZMn4/3ssceFbGEvuZaaheRM8ZmJJsqhtULaf/Ffiq24vcYU7KvSu', 'deleter', 'deleter');
+--
+-- insert into authority(name) values ('ROLE_ADMIN');
+--
+-- insert into authority(name) values ('ROLE_USER');
+--
+-- insert into authority(name) values ('ROLE_DELETER');
+--
+-- insert into user_authority (user_id, authority_id) values (1, 1);
+--
+-- insert into user_authority (user_id, authority_id) values (2, 2);
+--
+-- insert into user_authority (user_id, authority_id) values (3, 3);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,33 +1,27 @@
 DROP SEQUENCE IF EXISTS prescription_sequence;
 
-CREATE SEQUENCE prescription_sequence
-    START WITH 1
-    INCREMENT BY 1
-    MINVALUE 1;
+DROP TABLE IF EXISTS vaccine;
 
-DROP TABLE IF EXISTS medication_prescription;
+DROP TABLE IF EXISTS manufacturer;
 
-CREATE TABLE medication_prescription (
-                         id INT AUTO_INCREMENT,
-                         prescription_id INT AUTO_INCREMENT,
-                         prescription_name VARCHAR(100) NOT NULL,
-                         PRIMARY KEY (id),
-                         UNIQUE (prescription_id),
-                         vaccine_id INT NOT NULL,
-                         constraint fk_vaccine foreign key(vaccine_id) references vaccine(id)
+CREATE TABLE manufacturer (
+                              id INT AUTO_INCREMENT,
+                              manufacturer_name VARCHAR(100) NOT NULL,
+                              PRIMARY KEY (id),
+                              UNIQUE (manufacturer_name)
 );
 
-DROP TABLE IF EXISTS vaccine;
 
 CREATE TABLE vaccine (
                          id INT AUTO_INCREMENT,
                          research_name VARCHAR(100) NOT NULL,
-                         manufacturer_name VARCHAR(100) NOT NULL,
                          vaccine_type VARCHAR(100) NOT NULL,
                          required_number_of_shots INT NOT NULL,
                          available_number_of_shots INT NOT NULL,
+                         manufacturer_id INT NOT NULL,
                          PRIMARY KEY (id),
-                         UNIQUE (research_name)
+                         UNIQUE (research_name),
+                         constraint fk_manufacturer foreign key(manufacturer_id) references manufacturer(id)
 );
 
 DROP TABLE IF EXISTS side_effect;
@@ -43,33 +37,52 @@ CREATE TABLE side_effect (
                              constraint fk_vaccine foreign key(vaccine_id) references vaccine(id)
 );
 
-DROP TABLE IF EXISTS user;
 
-CREATE TABLE user (
-                      id INT AUTO_INCREMENT,
-                      username VARCHAR(100) NOT NULL,
-                      password VARCHAR(100) NOT NULL,
-                      first_name VARCHAR(100) NOT NULL,
-                      last_name VARCHAR(100) NOT NULL,
-                      PRIMARY KEY (id)
+CREATE SEQUENCE prescription_sequence
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1;
+
+DROP TABLE IF EXISTS medication_prescription;
+
+CREATE TABLE medication_prescription (
+                                         id INT AUTO_INCREMENT,
+                                         prescription_id INT AUTO_INCREMENT,
+                                         prescription_name VARCHAR(100) NOT NULL,
+                                         vaccine_id INT NOT NULL,
+                                         PRIMARY KEY (id),
+                                         UNIQUE (prescription_id),
+                                         constraint fk_vaccine_2 foreign key(vaccine_id) references vaccine(id)
 );
 
 
-DROP TABLE IF EXISTS authority;
-
-CREATE TABLE authority (
-                           id INT AUTO_INCREMENT,
-                           name VARCHAR(100) NOT NULL,
-                           PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS user_authority;
-
-CREATE TABLE user_authority (
-                                id INT AUTO_INCREMENT,
-                                user_id INT,
-                                authority_id INT,
-                                PRIMARY KEY (id),
-                                constraint fk_user foreign key(user_id) references user(id),
-                                constraint fk_authority foreign key(authority_id) references authority(id)
-);
+-- DROP TABLE IF EXISTS user;
+--
+-- CREATE TABLE user (
+--                       id INT AUTO_INCREMENT,
+--                       username VARCHAR(100) NOT NULL,
+--                       password VARCHAR(100) NOT NULL,
+--                       first_name VARCHAR(100) NOT NULL,
+--                       last_name VARCHAR(100) NOT NULL,
+--                       PRIMARY KEY (id)
+-- );
+--
+--
+-- DROP TABLE IF EXISTS authority;
+--
+-- CREATE TABLE authority (
+--                            id INT AUTO_INCREMENT,
+--                            name VARCHAR(100) NOT NULL,
+--                            PRIMARY KEY (id)
+-- );
+--
+-- DROP TABLE IF EXISTS user_authority;
+--
+-- CREATE TABLE user_authority (
+--                                 id INT AUTO_INCREMENT,
+--                                 user_id INT,
+--                                 authority_id INT,
+--                                 PRIMARY KEY (id),
+--                                 constraint fk_user foreign key(user_id) references user(id),
+--                                 constraint fk_authority foreign key(authority_id) references authority(id)
+-- );
